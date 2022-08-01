32beedf4c9d349a8569ba25a3da670df5390f1d0
Carlos A. Herrera <cherrera1208@gmail.com>
2022-08-01 16:45:11 -0700

    review event handler

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+'use strict';
+
+function Madlib(pronoun, noun, verb, adv, adj) {
+  this.pronoun = pronoun;
+  this.noun = noun;
+  this.verb = verb;
+  this.adv = adv;
+  this.adj = adj;
+}
+
+const form = document.querySelector('form');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let newMadlib = new Madlib(
+    event.target.pronoun.value,
+    event.target.noun.value,
+    event.target.verb.value,
+    event.target.adv.value,
+    event.target.adj.value
+  );
+  newMadlib.render();
+}
+
+form.addEventListener('submit', handleSubmit);
