f89e3944ca9c363c83a9b5894efa691b64607d2d
Jordan Yamada <sadaotomokane@gmail.com>
2022-08-01 17:04:34 -0700

    added local storage functions and randomizer function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 'use strict';
+
+//Global Variables
+
+
+
+//Object Constructors
+
+
+
+//Functional Logic
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+function updateStorage() {
+  const arrayString = JSON.stringify(userInputArr);
+  console.log(arrayString);
+  localStorage.setItem('input', arrayString);
+}
+
+function getStorage() {
+  // retrieve data from local storage
+  const data = localStorage.getItem('input');
+  // convert the data (array) from a string to something that we can use in JavaScript.
+  const inputData = JSON.parse(data);
+
+  // If this is the first time we visit the page, there will not be an array for us to use in localStorage
+  if (inputData !== null) {
+    userInputArr = inputData;
+    // }
+  }
+}
+
+
+
+//Executable Code
