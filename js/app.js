606a45c9d10f5ab039f0cf4bea52487590f10def
Jeffrey Smith <jeffrey_s_smith@live.com>
2022-08-01 17:18:20 -0700

    Global variable added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
 'use strict';
+// Global Variable
+
+let userInputArr = [];
+let madLibArr = ['London ${noun} Bridge is falling down ${verb} down "," falling down "." London Bridge is ${verb} down"," My fair ${noun}'];
+
+
